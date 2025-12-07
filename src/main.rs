e7590cdb4588d1824f7b2243b04469f43e3869ef
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-12-07 14:00:05 +0800

    修改：显示图片

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use fltk::{app, image::BmpImage, prelude::*, window::Window, draw, frame::Frame};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 800, 600, "Inkball Rust FLTK");
+
+    // 尝试加载 bmp 图像
+    let bmp_path = Path::new("assets/img.bmp");
+    let bmp_img = BmpImage::load(bmp_path);
+
+    let mut frame = Frame::new(0, 0, 800, 600, None);
+    if let Ok(img) = bmp_img {
+        frame.set_image(Some(img));
+    } else {
+        frame.set_label("无法加载 assets/img.bmp");
+    }
+
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
